f7d675514918dc5c5dd02ac87d1605af184bdc06
Marco Asbreuk <marco@itsasbreuk.nl>
2014-10-08 00:26:08 +0200

    intermediate saving

--- sh/production.sh
+++ sh/production.sh
@@ -3,7 +3,7 @@ cd ..
 jshint src/**/*.js
 jshint src/**/**/*.js
 yuidoc . -q -c ./site/yuidoc.json --themedir apitheme
-browserify -r ./src/itsa.build:itsa -u node-win > ./site/dist/itsabuild.js
+browserify -g cssify -r ./src/itsa.build:itsa -u node-win > ./site/dist/itsabuild.js
 uglifyjs ./site/dist/itsabuild.js -b ascii_only=true,beautify=false -c drop_debugger,drop_console,warnings=false -m >./site/dist/itsabuild-min.js
 cd ./site
 rm -f ./dist/itsabuild.tar
