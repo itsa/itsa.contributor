1081588f54f1f06e12ff2f080106ce3b8d470f04
Marco Asbreuk <marco@itsasbreuk.nl>
2014-11-10 00:15:21 +0100

    intermediate saving

--- sh/preview.sh
+++ sh/preview.sh
@@ -7,5 +7,5 @@ rm -f ./site-preview/dist/itsabuild.js
 rm -f ./site-preview/dist/itsabuild-min.js
 yuidoc . -q --themedir apitheme
 browserify -g cssify -r ./src/itsa.build:itsa -u node-win > ./site-preview/dist/itsabuild.js
-uglifyjs ./site-preview/dist/itsabuild.js -b ascii_only=true,beautify=false -c drop_debugger,drop_console,warnings=false -m >./site-preview/dist/itsabuild-min.js
+uglifyjs ./site-preview/dist/itsabuild.js -b ascii_only=true,beautify=false -c drop_debugger,drop_console,warnings=false,pure_funcs=['function_couter_get','function_couter_set'] -m >./site-preview/dist/itsabuild-min.js
 echo READY
